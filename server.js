0a9647d587a447a514927840f3ef72e8d8c086f6
Tooboi <japica123@gmail.com>
2023-01-06 01:29:16 -0500

    reworking post card

--- server.js
+++ server.js
@@ -3,21 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
 const helpers = require('./utils/helpers');
-// const bodyParser = require('body-parser');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const upload = require('./utils/multer');
-// const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Handlebars Helpers
 const hbs = exphbs.create({ helpers });
-
-
 
 // Cloudinary
 const cloudinary = require('cloudinary');
@@ -44,8 +38,6 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
